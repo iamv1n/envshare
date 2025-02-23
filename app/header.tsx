2ba4a19eee6996f94204649e7273d20a068281c6
Vineet <83384807+iamv1n@users.noreply.github.com>
2025-02-23 19:28:55 +0530

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -11,9 +11,8 @@ const navigation = [
   {
     name: "Unseal",
     href: "/unseal",
-    external: true,
   },
-] satisfies { name: string; href: string; external?: boolean }[];
+] satisfies { name: string; href: string }[];
 
 export const Header: React.FC = () => {
   const pathname = usePathname();
@@ -33,8 +32,6 @@ export const Header: React.FC = () => {
                     className={`flex items-center px-3 py-2 duration-150 text-sm sm:text-base  hover:text-zinc-50
                     ${pathname === item.href ? "text-zinc-200" : "text-zinc-400"}`}
                     href={item.href}
-                    target={item.external ? "_blank" : undefined}
-                    rel={item.external ? "noopener noreferrer" : undefined}
                   >
                     {item.name}
                   </Link>
