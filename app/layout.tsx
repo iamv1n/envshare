632320629c4f35b690efe56dbbefad8c2c68abb5
Vineet <83384807+iamv1n@users.noreply.github.com>
2025-02-23 19:25:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 
         <footer className="bottom-0 border-t inset-2x-0 border-zinc-500/10">
           <div className="flex flex-col gap-1 px-6 py-12 mx-auto text-xs text-center text-zinc-700 max-w-7xl lg:px-8">
-            <p>
+{/*             <p>
               Built by{" "}
               <Link href="https://twitter.com/chronark_" className="font-semibold duration-150 hover:text-zinc-200">
                 @chronark_
@@ -38,9 +38,9 @@ export default function RootLayout({
               >
                 many others{" "}
               </Link>
-            </p>
+            </p> */}
             <p>
-              EnvShare is deployed on{" "}
+              Share-Envs is deployed on{" "}
               <Link target="_blank" href="https://vercel.com" className="underline duration-150 hover:text-zinc-200">
                 Vercel
               </Link>{" "}
